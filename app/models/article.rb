a6c519650a6c8591f41d2abcb08c6b845709dea0
Albert Pai <albert@pai.email>
2016-02-23 15:43:59 -0800

    update comments json

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,8 @@
 class Article < ActiveRecord::Base
-  # has_many :post_tags
-  # has_many :tags, through: :post_tags
-
   belongs_to :user
   has_many :comments
+  has_many :favorites
 
-  # acts_as_taggable_on :tags
   acts_as_taggable
 
   validates :title, presence: true, allow_blank: false
--- app/views/comments/_comment.json.jbuilder
+++ app/views/comments/_comment.json.jbuilder
@@ -1,2 +1,2 @@
-json.(comment)
+json.(comment, :created_at, :updated_at, :body)
 json.author comment.user, partial: 'profiles/profile', as: :user
