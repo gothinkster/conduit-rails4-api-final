931f15d067699e1266a559593553819a9bb274ca
Albert Pai <albert@pai.email>
2016-03-03 13:28:49 -0800

    fix favs

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,7 @@ class Article < ActiveRecord::Base
   has_many :favorited_users, through: :favorites, source: :user
 
   scope :authored_by, ->(username) { where(user: User.where(username: username)) }
+  scope :favorited_by, -> (username) { joins(:favorites).where(favorites: { user: User.where(username: username) }) }
 
   acts_as_taggable
 
